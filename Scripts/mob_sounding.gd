0b92af44923e10f9e094bdb75b9dafe3e694a211
tobiasgronvall <tobiasgronvall94@gmail.com>
2024-04-14 23:03:09 +0200

    wops

--- Scripts/mob_sounding.gd
+++ Scripts/mob_sounding.gd
@@ -6,7 +6,7 @@ func _on_enemy_killed(type_info : enemy_type_info, points : int):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	GlobalEventSystem.connect(_on_enemy_killed)
+	GlobalEventSystem.monster_killed.connect(_on_enemy_killed)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
