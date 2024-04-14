8729d763a848543cf3cbe79b2c9d85da45d931c1
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2024-04-14 17:05:59 +0200

    Integrate custom heart animated

--- Scripts/UI/player_life_ui.gd
+++ Scripts/UI/player_life_ui.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @onready var _full_life_container = $FullLife
 @onready var _empty_life_container = $EmptyLife
+var _idle_timer := Timer.new()
 
 var _full_life = 0
 var _current_life = 0;
@@ -12,6 +13,11 @@ func _ready():
 	GlobalEventSystem.player_damaged.connect(_on_player_damaged)
 	_full_life = _full_life_container.get_child_count()
 	_current_life = _full_life
+	add_child(_idle_timer)
+	_idle_timer.wait_time = 5
+	_idle_timer.timeout.connect(self._play_animation)
+	_idle_timer.start()
+	_play_animation()
 
 func _on_player_damaged(damage : int):
 	_current_life = _current_life - damage
@@ -28,3 +34,7 @@ func _update_ui():
 	for i in range(_current_life, _full_life):
 		_full_life_container.get_child(i).visible = false
 		_empty_life_container.get_child(i).visible = true
+
+func _play_animation():
+	for i in range(0, _full_life):
+		_full_life_container.get_child(i).play("idle")
