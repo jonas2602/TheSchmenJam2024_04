500b5852a45ef0bf29e14727aee7e5dafa3a9026
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2024-04-14 11:39:18 +0200

    Start gameover screen

--- Scripts/UI/player_life_ui.gd
+++ Scripts/UI/player_life_ui.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var gameover_scene : PackedScene
+
 @onready var _full_life_container = $FullLife
 @onready var _empty_life_container = $EmptyLife
 
@@ -10,12 +12,13 @@ func _ready():
 	GlobalEventSystem.player_damaged.connect(_on_player_damaged)
 	_full_life = _full_life_container.get_child_count()
 	_current_life = _full_life
-	_current_life = max(_current_life, 0)
 
 func _on_player_damaged(damage : int):
 	_current_life = _current_life - damage
+	_current_life = max(_current_life, 0)
 	_update_ui()
-	pass
+	if (_current_life == 0):
+		get_tree().change_scene_to_file(gameover_scene.resource_path)
 
 func _update_ui():
 	for i in range(0, _current_life):
