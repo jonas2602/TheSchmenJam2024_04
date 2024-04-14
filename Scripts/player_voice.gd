8b33b1e5ec44702313400eb6a3e546dfdef60825
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2024-04-14 08:50:03 +0200

    small refacto on voice input to use event system

--- Scripts/player_voice.gd
+++ Scripts/player_voice.gd
@@ -3,46 +3,33 @@ extends Node
 const MAX_INT : int = 0xFFFFFFFF
 const NO_KEY  : int = MAX_INT
 
-var streams = {}
-
-var player : AudioStreamPlayer
+var _streams = {}
+var _last_pressed_key = ""
+@onready var _audio_player = $AudioStreamPlayer
 
 func load_sounds():
 	var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	for k in range(KEY_A, KEY_Z+1):
-		streams[k] = ResourceLoader.load("res://Data/Audio/Alphabet/" + chars[k-KEY_A] + ".ogg")
+		_streams[chars[k-KEY_A]] = ResourceLoader.load("res://Data/Audio/Alphabet/" + chars[k-KEY_A] + ".ogg")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = $AudioStreamPlayer
 	load_sounds()
+	GlobalEventSystem.input_detected.connect(_on_input_detected)
 	
-var last_pressed_key : int = NO_KEY
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	var stream : AudioStream = null
-	
-	var pressed_key = NO_KEY
-	for k in range(KEY_A, KEY_Z+1):
-		if (Input.is_key_pressed(k)):
-			pressed_key = k
-	
-	if pressed_key == last_pressed_key:
+func _on_input_detected(input_char : String):
+	if input_char == _last_pressed_key:
 		return
 	
-	if (pressed_key == NO_KEY):
-		last_pressed_key = pressed_key
-		return
-	
-	stream = streams[pressed_key]
+	var stream = _streams.get(input_char[0])
 	if (!stream):
 		return
 	
-	if player.is_playing:
-		player.stop()
+	if _audio_player.is_playing:
+		_audio_player.stop()
 	
-	player.stream = stream
-	player.play()
+	_audio_player.stream = stream
+	_audio_player.play()
 	
-	last_pressed_key = pressed_key
+	_last_pressed_key = input_char
 	
