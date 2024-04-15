2e8bf4401c2428d94fe081cb3f5a41ff4a462e0c
Jonas Schneider <jonas.schneider96@freenet.de>
2024-04-15 22:33:56 +0200

    - added option to escape from credits - added special thanks text - added logo to menu screen

---
+++ Scripts/UI/credits_ui.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalEventSystem.restart.connect(_on_restart)
+	pass # Replace with function body.
+
+func _on_restart(credits : bool):
+	visible = credits
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
